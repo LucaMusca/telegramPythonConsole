0a5ec9e7554a3252298e5d85b7e8582e6028c6a6
LucaMusca <57320146+LucaMusca@users.noreply.github.com>
2020-12-20 12:21:49 +0100

    Create matplotlib_backend.py

---
+++ matplotlib_backend.py
@@ -0,0 +1,34 @@
+import threading
+import os
+import telegram
+from matplotlib.backend_bases import Gcf
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+import mainip
+
+FigureCanvas = FigureCanvasAgg
+
+manager, myChatID, bot = None, None, None
+
+
+def show(*args, **kwargs):
+    for num, figmanager in enumerate(Gcf.get_all_fig_managers()):
+        pid = threading.get_ident()
+        try:
+            consoleID = manager.threads[pid].chatID
+        except KeyError:
+            consoleID = myChatID
+        filename = '%s.png' % consoleID
+        figmanager.canvas.figure.savefig(filename)
+        sendPhoto(consoleID, filename)
+        os.remove(filename)
+
+
+def sendPhoto(chatID, filename):
+    console = manager.data[chatID]
+    console.history.index = 0
+    try:
+        bot.edit_message_reply_markup(chat_id=console.lastMessage.chat_id, message_id=console.lastMessage.message_id)
+    except (telegram.error.BadRequest, AttributeError):
+        pass
+    reply_markup = console.history()
+    console.lastMessage = bot.send_photo(chat_id=chatID, photo=open(filename, 'rb'),reply_markup=reply_markup)
